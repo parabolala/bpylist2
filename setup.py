689edee965138b5697eaa7c11e4176071a56d8be
Thomas Bartelmess <tbartelmess@marketcircle.com>
2017-09-12 15:29:17 -0400

    Include the README

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import io
+import os
 from setuptools import setup
 from distutils.core import Extension
 
 bplist = Extension('bpylist.bplist', sources=['src/bplist.c'])
 
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
 setup(
     name='bpylist',
-    version='0.1.0',
+    version='0.1.1',
+    description = "parse and generate binary plists and NSKeyedArchiver archives",
     author='Marketcircle Inc.',
     author_email='cloud@marketcircle.com',
     url='https://github.com/marketcircle/bpylist',
