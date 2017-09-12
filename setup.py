503b4582414c4bde64fa91e71cf2c74c507953fe
Thomas Bartelmess <tbartelmess@marketcircle.com>
2017-09-12 15:38:13 -0400

    Added a long description

--- setup.py
+++ setup.py
@@ -14,8 +14,9 @@ with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='bpylist',
-    version='0.1.2',
+    version='0.1.3',
     description = "parse and generate binary plists and NSKeyedArchiver archives",
+    long_description = long_description,
     author='Marketcircle Inc.',
     author_email='cloud@marketcircle.com',
     url='https://github.com/marketcircle/bpylist',
