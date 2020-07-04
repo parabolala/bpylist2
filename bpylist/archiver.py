c3a077cc2b35a5e1b375469f13a56cfbf8486539
Rhet Turnbull <rturnbull+git@gmail.com>
2020-07-04 10:40:36 -0700

    Synched to bpylist2 upstream

--- bpylist/archiver.py
+++ bpylist/archiver.py
@@ -1,7 +1,8 @@
+import plistlib
+
 from typing import Mapping, Dict
 
-from bpylist import bplist  # type: ignore
-from bpylist.archive_types import timestamp, uid, NSMutableData
+from bpylist.archive_types import timestamp, NSMutableData
 
 # The magic number which Cocoa uses as an implementation version.
 # I don' think there were 99_999 previous implementations, I think
@@ -9,7 +10,7 @@ from bpylist.archive_types import timestamp, uid, NSMutableData
 NSKeyedArchiveVersion = 100_000
 
 # Cached for convenience
-null_uid = uid(0)
+NULL_UID = plistlib.UID(0)
 
 
 def unarchive(plist: bytes) -> object:
@@ -137,7 +138,7 @@ class ArchivedObject:
         self.object = obj
         self._unarchiver = unarchiver
 
-    def decode_index(self, index: uid):
+    def decode_index(self, index: plistlib.UID):
         return self._unarchiver.decode_object(index)
 
     def decode(self, key: str):
@@ -146,7 +147,6 @@ class ArchivedObject:
 
 class CycleToken:
     "token used in Unarchive's unpacked_uids cache to help detect cycles"
-    pass
 
 
 class Unarchive:
@@ -170,12 +170,12 @@ class Unarchive:
 
     def __init__(self, input_bytes: bytes) -> None:
         self.input = input_bytes
-        self.unpacked_uids: Dict[int, object] = {}
-        self.top_uid = null_uid
+        self.unpacked_uids: Dict[plistlib.UID, object] = {}
+        self.top_uid = NULL_UID
         self.objects: list = []
 
     def unpack_archive_header(self):
-        plist = bplist.parse(self.input)
+        plist = plistlib.loads(self.input)
 
         archiver = plist.get('$archiver')
         if archiver != 'NSKeyedArchiver':
@@ -189,18 +189,19 @@ class Unarchive:
         if not isinstance(top, dict):
             raise MissingTopObject(plist)
 
-        self.top_uid = top.get('root')
-        if not isinstance(self.top_uid, uid):
+        top_uid = top.get('root')
+        if top_uid is None:
             raise MissingTopObjectUID(top)
+        self.top_uid = top_uid
 
         self.objects = plist.get('$objects')
         if not isinstance(self.objects, list):
             raise MissingObjectsArray(plist)
 
-    def class_for_uid(self, index: uid):
+    def class_for_uid(self, index: plistlib.UID):
         "use the UNARCHIVE_CLASS_MAP to find the unarchiving delegate of a uid"
 
-        meta = self.objects[index]
+        meta = self.objects[index.data]
         if not isinstance(meta, dict):
             raise MissingClassMetaData(index, meta)
 
@@ -216,14 +217,14 @@ class Unarchive:
 
     def decode_key(self, obj, key):
         val = obj.get(key)
-        if isinstance(val, uid):
+        if isinstance(val, plistlib.UID):
             return self.decode_object(val)
         return val
 
-    def decode_object(self, index: uid):
+    def decode_object(self, index: plistlib.UID):
         # index 0 always points to the $null object, which is the archive's
         # special way of saying the value is null/nil/none
-        if index == 0:
+        if index == NULL_UID:
             return None
 
         obj = self.unpacked_uids.get(index)
@@ -233,7 +234,7 @@ class Unarchive:
         if obj is not None:
             return obj
 
-        raw_obj = self.objects[index]
+        raw_obj = self.objects[index.data]
 
         # put a temp object in place, in case we have a circular
         # reference, which we do not really support
@@ -245,7 +246,7 @@ class Unarchive:
             return raw_obj
 
         class_uid = raw_obj.get('$class')
-        if not isinstance(class_uid, uid):
+        if class_uid is None:
             raise MissingClassUID(raw_obj)
 
         klass = self.class_for_uid(class_uid)
@@ -292,7 +293,7 @@ class Archive:
     """
 
     # types which do not require the "object" encoding for an archive;
-    primitive_types = [int, float, bool, str, bytes, uid]
+    primitive_types = [int, float, bool, str, bytes, plistlib.UID]
 
     # types which require no extra encoding at all, they can be inlined
     # in the archive
@@ -307,7 +308,7 @@ class Archive:
         # objects that go directly into the archive, always start with $null
         self.objects = ['$null']
 
-    def uid_for_archiver(self, archiver: type) -> uid:
+    def uid_for_archiver(self, archiver: type) -> plistlib.UID:
         """
         Ensure the class definition for the archiver is included in the arcive.
 
@@ -324,7 +325,7 @@ class Archive:
         if val:
             return val
 
-        val = uid(len(self.objects))
+        val = plistlib.UID(len(self.objects))
         self.class_map[archiver] = val
 
         # TODO: this is where we might need to include the full class ancestry;
@@ -392,11 +393,11 @@ class Archive:
             archive_wrapper = ArchivingObject(archive_obj, self)
             cls.encode_archive(obj, archive_wrapper)
 
-    def archive(self, obj) -> uid:
+    def archive(self, obj) -> plistlib.UID:
         "Add the encoded form of obj to the archive, returning the UID of obj."
 
         if obj is None:
-            return null_uid
+            return NULL_UID
 
         # the ref_map allows us to avoid infinite recursion caused by
         # cycles in the object graph by functioning as a sort of promise
@@ -404,7 +405,7 @@ class Archive:
         if ref:
             return ref
 
-        index = uid(len(self.objects))
+        index = plistlib.UID(len(self.objects))
         self.ref_map[id(obj)] = index
 
         cls = obj.__class__
@@ -429,10 +430,10 @@ class Archive:
             '$archiver': 'NSKeyedArchiver',
             '$version': NSKeyedArchiveVersion,
             '$objects': self.objects,
-            '$top': {'root': uid(1)}
+            '$top': {'root': plistlib.UID(1)}
         }
-
-        return bplist.generate(d)
+        return plistlib.dumps(
+            d, fmt=plistlib.FMT_BINARY)  # pylint: disable=no-member
 
 
 UNARCHIVE_CLASS_MAP = {
