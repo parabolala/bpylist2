0bdc15e56553f7d680374cb84ac42c215878299d
Rhet Turnbull <rturnbull+git@gmail.com>
2020-07-04 12:53:41 -0700

    Added support for python 3.6, 3.7

--- setup.py
+++ setup.py
@@ -6,20 +6,18 @@ import os
 from setuptools import setup  # type: ignore
 
 here = os.path.abspath(os.path.dirname(__file__))
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
 setup(
-    name='bpylist2',
-    version='3.0.1',
+    name="bpylist2",
+    version="3.0.2",
     description=("parse and generate NSKeyedArchiver archives"),
     long_description=long_description,
-    author='Marketcircle Inc., Ievgen Varavva',
-    author_email='fuzzy.parabola@gmail.com',
-    url='https://github.com/xa4a/bpylist2',
-    packages=[
-        'bpylist',
-    ],
+    author="Marketcircle Inc., Ievgen Varavva",
+    author_email="fuzzy.parabola@gmail.com",
+    url="https://github.com/xa4a/bpylist2",
+    packages=["bpylist"],
     setup_requires=[
         "pytest-runner",
         "pytest-pylint",
@@ -28,18 +26,16 @@ setup(
         'dataclasses;python_version<"3.7"',
     ],
     tests_require=["pytest"],
-    install_requires=[
-        'dataclasses;python_version<"3.7"',
-    ],
+    install_requires=['dataclasses;python_version<"3.7"'],
     include_package_data=True,
-    test_suite='tests',
+    test_suite="tests",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python :: 3.8',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries'
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
     ],
-
     # 3.8 required for plistlib.UID.
-    python_requires=">=3.8",
+    # local copy of plistlib.py included for python < 3.8
+    python_requires=">=3.6",
 )
